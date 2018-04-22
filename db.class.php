289feb08bb71805b673fccb5f70b153d8b2b3b42
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2018-04-22 20:20:16 -0300

    Update db.class.php

--- db.class.php
+++ db.class.php
@@ -4,9 +4,12 @@ define('PASS','');
 define('HOST','localhost');
 define('DBNAME','teste');
 define('TYPEBD','mysql');
+define('FILE_DATE_MODIFIED', 'file_last_execution.pid');
 
 class Db{
-private $conn, $table = 'users', $logfile = 'LogDb.txt', $numbers_indices_search  = array(), $name_indices_search = array(), $search_params= array();
+private $conn, $table = 'users', $logfile = 'LogDb.txt', $numbers_indices_search  = array(), 
+$name_indices_search = array(), $search_params = array(), $camps = '', $distinct = false, $indiceOR = array(), $relationship = array();
+
 function __construct(){
 		$this->getConn();
 	}
@@ -18,36 +21,52 @@ function getConn(){
 	$this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 	}
-	
 	return $this->conn;
 	}catch(Exception $e){	
 	   $this->writeError($e);	
 	}
 }
+
 public function setTable($table){
 	$this->table = $table;
 	}
 	
 function writeError($e){
-	echo 'Erro -> ';
 	$errorx = $e->getMessage();
-    var_dump($errorx);	
 	$fp = fopen($this->logfile, 'a');
 	$write = fwrite($fp,date("d-m-Y H:i:s ").": Erro-> ");
 	$write = fwrite($fp,$errorx.PHP_EOL.PHP_EOL);
 	fclose($fp);
+	$data['error'] = 'Erro -> '.$errorx;
+	echo json_encode($data);
+	exit();
 }	
-		
-public function getTable(){ return $this->table;}
 
+public function writeDataIntoFile(){
+	$op = fopen(FILE_DATE_MODIFIED, 'w');
+	fwrite($op, date('Y-m-d H:i:s'));
+	fclose($op);	
+}
+
+public function getFileDataLast(){
+	$data = "";	
+	if(file_exists(FILE_DATE_MODIFIED)){
+	$lines = file(FILE_DATE_MODIFIED);
+	foreach($lines as $line)
+	{
+	$data = $line;
+	}
+	}
+	return $data;
+	}
+			
+public function getTable(){ return $this->table;}
 public function getNumbersIndicesSearch(){return $this->numbers_indices_search;}
 public function setNumbersIndicesSearch($indicesSearch){$this->numbers_indices_search[] = $indicesSearch;}
 public function resetNumbersIndicesSearch(){$this->numbers_indices_search = array();}
 public function setNameIndicesNumbers($ind, $value){ $this->name_indices_search[$ind] = $value;}
 public function getNameIndicesNumbers($ind){ return $this->name_indices_search[$ind];} 
 public function resetNameIndicesNumbers(){$this->name_indices_search = array();}
-
-
 public function setSearchParams($searchs = array()){
 	if(is_array($searchs)){
 	$this->search_params = $searchs;
@@ -59,8 +78,12 @@ public function getSearchParams(){
 public function addSearchParams($val){
 	$this->search_params[] = $val;
 	}
-
 public function resetSearchParams(){$this->search_params = array();}
+
+public function setSearchString($strSearch){
+	$strSearch = explode(',', $strSearch);
+	$this->setSearchParams($strSearch);
+	}
 
 public function isIndiceSearch($ind){
 	$verify = false;
@@ -70,24 +93,80 @@ public function isIndiceSearch($ind){
 		}
 	return $verify;
 	}
+	
+public function setCamps($params){ $this->camps = $params;}
+public function addCamps($value){ if($this->camps <> '') $this->camps = $value; else $this->camps .= $value; }
+public function getCamps(){ return $this->camps; }
+public function resetCamps(){ $this->camps = "";}
 
+public function getSqlCamps(){
+	$val = "*";
+	if(trim($this->camps) <> '') $val = $this->getCamps();
+	if($this->getDistinct()){  $val = " DISTINCT ".$val;   }
+	return $val;
+	}
+	
+public function setDistinct(){ $this->distinct = true; }
+public function getDistinct(){ return $this->distinct;}
+public function resetDistinct(){ $this->setDistinct(false);}
+public function getIndicesOR(){ return $this->indiceOR;}
+public function setIndicesOR($inds = array()){   if(is_array($inds)) $this->indiceOR = $inds; else $this->indiceOR = array();    }
+public function resetIndicesOR(){ $this->setIndicesOR();}
+
+public function setIndicesOrString($value){
+	$strIndiceOr = explode(',',$value);
+	$this->setIndicesOR($strIndiceOr);
+	}
+
+public function verifyIsAndOR($value){
+	$resultado = " AND ";
+	foreach($this->indiceOR as $inds){
+		if($inds == $value){ $resultado = " OR ";}
+		}
+return $resultado ;	
+}
+
+public function addRelationship($table_with_alias = "" , $connected_camps, $joinType = " INNER "){
+	$sql_relationship = " ".trim($joinType)." JOIN ".$table_with_alias." ON ".$connected_camps." ";
+	$this->relationship[] = $sql_relationship;
+}
+
+public function getRelationships(){
+	return $this->relationship;
+	}
+
+public function getRelationshipsString(){
+	$string_relationships = "";
+	foreach($this->getRelationships() as $relat){
+		$string_relationships .= " ".$relat." ";
+		}
+	return $string_relationships;
+	}
+	
+public function resetRelationship(){
+	$this->relationship =  array();
+	}
+	
 private function getStr($data){
 	$dt =  array();
 	$indices = is_array($data)? array_keys($data) : array();
 	$indices_search = $this->getSearchParams();
+	
 	$dt['ind'] = $indices;
 	$count = count($indices);
+	
 	$dt['count'] = $count; 
     $num = 0;
     $dt['str'] = '';
 	$dt['qst'] = '';
     $dt['cmb'] = '';
 	$dt['read'] = '';
+	
     foreach($indices as $ind){
 	$dt['str'] .=$ind;
 	$dt['qst'] .='?';
 	$dt['cmb'] .= $ind.'=?';
-	$dt['read'] .= (!$this->verifyIfIsSearch($indices_search, $ind) && count($this->getSearchParams()) == 0) ? $ind.'=?' : $ind." LIKE ?";
+	$dt['read'] .= (!$this->verifyIfIsSearch($indices_search, $ind)) ? $ind.'=?' : $ind." LIKE ?";
 	
 	if($this->verifyIfIsSearch($indices_search, $ind)){ 
 	$index_pesquisa = $num + 1;
@@ -99,7 +178,10 @@ private function getStr($data){
 	$dt['str'] .=','; 
 	$dt['qst'] .=',';
 	$dt['cmb'] .=',';
-	$dt['read'] .= ' AND ';	
+	
+	$valor_next = $indices[$num+1];
+	
+	$dt['read'] .= $this->verifyIsAndOR($valor_next);	
 	}
 	$num++;
 	}
@@ -118,6 +200,16 @@ function prepareSql($pdo, $dta,$dx){
 	}
 	}
 	
+function Reset(){
+	$this->resetNumbersIndicesSearch();
+	$this->resetNameIndicesNumbers();
+	$this->resetSearchParams();
+	$this->resetCamps();
+	$this->resetDistinct();
+	$this->resetIndicesOR();
+	$this->resetRelationship();
+	}	
+	
 function prepareSql2($pdo, $dta,$dx){
 	$count = is_array($dx)? $dx['count'] : 0;
 	$count2 = count($dta);
@@ -128,13 +220,9 @@ function prepareSql2($pdo, $dta,$dx){
 		}
 	$pdo->execute();  
 	}
-	$this->resetNumbersIndicesSearch();
-	$this->resetNameIndicesNumbers();
-	$this->resetSearchParams();
+	$this->Reset();
 	return $pdo;
 	}	
-
-
 public function verifyIfIsSearch($indices_search, $value){
 	$search = false;
 	if(is_array($indices_search)){
@@ -150,10 +238,12 @@ public function verifyIfIsSearch($indices_search, $value){
 function getVarTp($indice,  $value, $pdo){
 	$tp_data = gettype($value);
 	$isSearchValue = $this->isIndiceSearch($indice);
-	if($isSearchValue == true){	
+	
+	if($isSearchValue === true){	
         $search = "%".(string)$value."%";
 		$pdo->bindValue($indice,$search , PDO::PARAM_STR );	
     }else{
+		
 	switch($tp_data){
 		case "integer":
 		$pdo->bindValue($indice, $value, PDO::PARAM_INT );
@@ -219,11 +309,8 @@ public function Read($where = '', $table = '', $limit = "",$orderby = ""){
 	$tab = trim($table) == '' ? $this->table : $table;
 	$sql = "SELECT * FROM ".$tab;
 	$sql = strip_tags(trim($where == ''))? $sql : $sql." WHERE ".$where;
-	
-	
 	$sql = (trim($orderby) <> '')? $sql." ORDER BY ".$orderby : $sql; 
-	$sql = (trim($limit) <> '') ? $sql.$limit  :$sql ;
-	
+	$sql = (trim($limit) <> '') ? $sql.$limit  :$sql ;	
 	$pdo = $this->getConn()->query($sql);
 	$pdo->setFetchMode(PDO::FETCH_ASSOC);
 	return $pdo->fetchAll();
@@ -232,32 +319,45 @@ public function Read($where = '', $table = '', $limit = "",$orderby = ""){
 		}
 	}
 	
-public function ReadPdo($where = array(), $table = '', $limit = "",$orderby = "", $search_params = array()){
+public function ReadPdo($where = array(), $table = '', $limit = "",$orderby = "", $groupby = ""){
 	try{
 	$tab = trim($table) == '' ? $this->table : $table;
-	$sql = "SELECT * FROM ".$tab." ";
+	$sql = "SELECT ".$this->getSqlCamps()." FROM ".$tab." ";
 	$strWher =  is_array($where) && count($where) > 0 ? $this->getStr($where) : '';
 	$str_where = "";
 	
 	if((is_array($where) && strpos($strWher['read'], "WHERE"))){
-		$str_where = $strWher['read'];
+		$str_where =  ($strWher['read'] <> '')?  $strWher['read']  : '';
 		}
 	else if(is_array($where) && !(strpos($strWher['read'], "WHERE"))){
-		$str_where = " WHERE ".$strWher['read'];
+		$str_where = (count($where) > 0) ? " WHERE ".$strWher['read'] : '';
 		}
 		
 	else if(!(is_array($where)) && !(strpos($where, "WHERE")) && trim($where) <> ''){
-		$str_where = " WHERE ".$where;
+		$str_where = (trim($where) <>  '')? " WHERE ".$where : '';
 		}
+		
+	$sql .= $this->getRelationshipsString();	
 	$sql .= $str_where;
-	
-	$sql .= $orderby.' '.$limit;
+
+	$groupby = trim($groupby) <> '' && !(strpos(strtoupper($groupby), 'GROUP BY')) ? " GROUP BY ".$groupby : $groupby;
+	$orderby = trim($orderby) <> '' && !(strpos(strtoupper($orderby),'ORDER BY'))  ? " ORDER BY ".$orderby : $orderby;
+	$limit   = trim($limit) <> ''   && !(strpos(strtoupper($limit),'LIMIT')) ?  " LIMIT ".$limit : $limit;
+	$sql .= $groupby.$orderby.' ';
+	$sql_all = $sql;
+	$sql .= $limit <> '' ? $limit : '';
 	
 	$pdo = $this->getConn()->prepare($sql);
 	$pdo = $this->prepareSql2($pdo,$where,$strWher);
 	$pdo->setFetchMode(PDO::FETCH_ASSOC);
 	$dat = array();
+	
+	$pdo2 = $this->getConn()->prepare($sql_all);
+	$pdo2 = $this->prepareSql2($pdo,$where,$strWher);
+	$pdo2->setFetchMode(PDO::FETCH_ASSOC);
+	
 	$dat['count'] = $pdo->rowCount();
+	$dat['count_all'] = $pdo2->rowCount();
 	$dat['data'] = $pdo->fetchAll();
     return $dat;
 	
@@ -293,13 +393,10 @@ function create_guid()
 {
     $microTime = microtime();
     list($a_dec, $a_sec) = explode(' ', $microTime);
-
     $dec_hex = dechex($a_dec * 1000000);
     $sec_hex = dechex($a_sec);
-
     $this->ensure_length($dec_hex, 5);
     $this->ensure_length($sec_hex, 6);
-
     $guid = '';
     $guid .= $dec_hex;
     $guid .= $this->create_guid_section(3);
@@ -312,21 +409,17 @@ function create_guid()
     $guid .= '-';
     $guid .= $sec_hex;
     $guid .= $this->create_guid_section(6);
-
     return $guid;
 }	
 	
-
 public function create_guid_section($characters)
 {
     $return = '';
     for ($i = 0; $i < $characters; ++$i) {
         $return .= dechex(mt_rand(0, 15));
     }
-
     return $return;
 }
-
 public function ensure_length(&$string, $length)
 {
     $strlen = strlen($string);
@@ -336,7 +429,6 @@ public function ensure_length(&$string, $length)
         $string = substr($string, 0, $length);
     }
 }
-
 public function microtime_diff($a, $b)
 {
     list($a_dec, $a_sec) = explode(' ', $a);
@@ -345,3 +437,5 @@ public function microtime_diff($a, $b)
 }
 	
 }
+
+?>
