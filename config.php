13609b74e0f7639e3cf4bfb9a35388829b0955dc
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2018-05-20 15:35:53 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('USER', 'root');
+define('PASS','');
+define('HOST','localhost');
+define('DBNAME','teste');
+define('TYPEBD','mysql');
+define('FILE_DATE_MODIFIED', 'arquivo_ult_execucao.txt');
+?>
